7c9006ee93b0e89d63eba9cb39ea3a7a724cbf1f
Luis Gustavo <lgsc2109@gmail.com>
2020-11-02 21:56:17 -0500

    trim fields function

--- src/app/services/action/action.service.ts
+++ src/app/services/action/action.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@angular/core';
 import Swal from "sweetalert2";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ActionService {
 
-  constructor() { }
+    constructor() { }
 
     /**
      * Este metodo abre el Swal alert
